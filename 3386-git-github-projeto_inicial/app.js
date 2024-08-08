3880e33e7fcfb6121603a09ee44744d40eff177a
Mirela-Viana <mirelaviana.melo@gmail.com>
2024-08-08 15:47:53 -0300

    Modificação numeroLimite e Chave

--- 3386-git-github-projeto_inicial/app.js
+++ 3386-git-github-projeto_inicial/app.js
@@ -1,9 +1,9 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 20;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
-function exibirTextoNaTela(tag, texto) {
+function exibirTextoNaTela(tag, texto) 
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
